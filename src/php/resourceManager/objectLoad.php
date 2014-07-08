1102bf1d4ea45bc399f38fa886bbfc0cd83673d1
vinhquang <vinhquang09@gmail.com>
2014-07-08 10:50:25 +0700

    Update json beautifier

--- src/php/resourceManager/objectLoad.php
+++ src/php/resourceManager/objectLoad.php
@@ -2,6 +2,6 @@
 
 $objectFilePath = 'object.js';
 
-$data = str_replace('res/', '../engine/res/', file_get_contents($objectFilePath));
+$data = str_replace('res\/', '..\/engine\/res\/', file_get_contents($objectFilePath));
 
 die($data);
